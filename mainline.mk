91067c02a7304a58f23550ec1b82499df190c8e6
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    mainline-common: Adding GSI keys
    Bug: 112293933
    Test: tree hugger
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- mainline.mk
+++ mainline.mk
@@ -9,6 +9,9 @@ PRODUCT_SHIPPING_API_LEVEL := 32
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
