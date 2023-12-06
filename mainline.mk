43e8449a9b118481af0077b430a4842633533f55
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-07 00:48:59 +0100

    mainline-common: Build basic USB HIDL
    Change-Id: I9339ddc96be6156e9a78b6b960122aba886380df

--- mainline.mk
+++ mainline.mk
@@ -198,3 +198,11 @@ endif
 # Thermal
 PRODUCT_PACKAGES += \
     com.android.hardware.thermal
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.basic
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
