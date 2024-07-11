673e0dce732a0712ff95bcbfa180f45549ccf0b1
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-12 00:25:04 +0200

    [TEMP]
    Change-Id: I1d96d147b4793fca945a170f69879d8bac2cfee6

--- mainline.mk
+++ mainline.mk
@@ -46,8 +46,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.v4l2
+    android.hardware.camera.provider@2.4-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
