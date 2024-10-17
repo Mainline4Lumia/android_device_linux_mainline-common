09a575fd1e3afaa1d83528482fd61b2928467ead
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-10-17 04:10:06 +0200

    mainline-common: Build example light AIDL service
    Change-Id: I74070bb1fe115d61dd17c928aec2ee17a0bfb280

--- mainline.mk
+++ mainline.mk
@@ -145,6 +145,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.example
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
