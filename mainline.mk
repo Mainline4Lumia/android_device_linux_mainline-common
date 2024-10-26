5db5c01a0779525e03b824c75858f0cbe16d5d82
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-10-26 23:55:43 +0200

    mainline-common: Advertise Ethernet feature
    Change-Id: I8dffa87c71971133b349b63679699222faed5cfa

--- mainline.mk
+++ mainline.mk
@@ -132,6 +132,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Ethernet
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
